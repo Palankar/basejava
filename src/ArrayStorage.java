c1e6a13583893dc4a3060760c1f0072a42d589bf
Palankar <Palankar7@yandex.ru>
2019-09-21 22:45:20 +0300

    Implementation methods of ArrayStorage class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,64 @@
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
 
     void clear() {
+        storage = null;
+        storage = new Resume[10000];
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (Resume r: storage) {
+            if (!Objects.isNull(r) && r.uuid.equals(uuid)) {
+                resume = r;
+                break;
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size() - 1; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                break;
+            }
+        }
+        Resume[] newRes = getAll();
+        clear();
+        System.arraycopy(newRes, 0, storage, 0, newRes.length);
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allRes = new Resume[size()];
+        int count = 0;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (!Objects.isNull(storage[i])) {
+                allRes[count] = storage[i];
+                count++;
+            }
+        }
+        return allRes;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (!Objects.isNull(resume))
+                count++;
+        }
+        return count;
     }
 }
